5578591d5b885690acf68e2333a4f033e1ad72cc
aiXpertLab <aiXpertLab@gmail.com>
2024-04-27 23:18:40 -0400

    init

--- Home.py
+++ Home.py
@@ -1,7 +1,9 @@
 import streamlit as st, pathlib, os, openai, tiktoken
 import utilities as ut
+import datetime
 
 st.set_page_config(page_title="easyStorage: Low-cost storage near you!",  page_icon="🚚",)
 st.image('eslogo.png', use_column_width=True)
 st.write("# 🚚 Admin 🚀")
 
+st.code(datetime.datetime.now().date())
--- pages/1_📊Update OpenAI API Key.py
+++ pages/1_📊Update OpenAI API Key.py
@@ -4,79 +4,76 @@ from cryptography.fernet import Fernet
 import pymongo
 import base64
 
-st_def.st_logo(title='Welcome 👋 to Book Summarizer!', page_title="PDF Summarizer",)
-st_def.st_load_book()
 
-# Your key
-key_bytes = Fernet.generate_key()
+st.image('eslogo.png', use_column_width=True)
+st.write("# 🚚 Admin 🚀")
 
-# Convert bytes key to base64 encoded string
-key_string = base64.urlsafe_b64encode(key_bytes).decode()
+password = st.text_input("Copy/Paste the OpenAI API Key, and press Enter", type="password")
 
-# Save the key to a file
-with open('key.txt', 'w') as keyfile:
-    keyfile.write(key_string)
-    
+if password:
+    st.code("You entered: " + password[:22] + "...")
+    # Your key
+    # key_bytes = Fernet.generate_key()
 
+    # # Convert bytes key to base64 encoded string
+    # key_string = base64.urlsafe_b64encode(key_bytes).decode()
 
-uri = "mongodb+srv://easystorage:wbS4VPXsLur59ETT@easystorage.q0y7eu5.mongodb.net/easystorage?retryWrites=true&w=majority&appName=easystorage"
-cipher_suite = Fernet(key_bytes)
+    # # Save the key to a file
+    # with open('key.txt', 'w') as keyfile:
+    #     keyfile.write(key_string)
 
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
-def init_connection():
-    return pymongo.MongoClient(uri)
+    uri = st.secrets["MONGO_URI"]
 
-client = init_connection()
+    @st.cache_resource
+    def init_connection():
+        return pymongo.MongoClient(uri)
 
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-    mydatabase = client.easystorage
-    mycollection = mydatabase.openai
-    print(mycollection)
-except Exception as e:
-    print(e)
+    client = init_connection()
 
-# Pull data from the collection.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def get_data():
-    items = mycollection.find()
-    items = list(items)  # make hashable for st.cache_data
-    return items
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        mydatabase = client.easystorage
+        mycollection = mydatabase.openai
+        print(mycollection)
+    except Exception as e:
+        print(e)
 
-items = get_data()
+    # Pull data from the collection.
+    # Uses st.cache_data to only rerun when the query changes or after 10 min.
+    @st.cache_data(ttl=600)
+    def get_data():
+        items = mycollection.find()
+        items = list(items)  # make hashable for st.cache_data
+        return items
 
-# Print results.
-for item in items:
-    st.write(item)
-    st.write(f"{item['0']['name']} has a :{item['0']['value']}:")
+    items = get_data()
 
-value = cipher_suite.encrypt('da33ta'.encode())
-filter_query = {"0.name": "key"}
-update_query = {"$set": {"0.value": value}}
+    st_key = st.secrets["FERNET"]
+    key_bytes = base64.urlsafe_b64decode(st_key)
+    cipher_suite = Fernet(key_bytes)
+    value = cipher_suite.encrypt(password.encode())
+    filter_query = {"0.name": "key"}
+    update_query = {"$set": {"0.value": value}}
 
-# Update the document
-result = mydatabase.openai.update_one(filter_query, update_query)
+    # Update the document
+    result = mydatabase.openai.update_one(filter_query, update_query)
 
-# Check if the update was successful
-if result.modified_count > 0:
-    print("Update successful!")
-else:
-    print("No documents matched the filter.")
-    
-    
-query = {"0.name": "key"}
+    # Check if the update was successful
+    if result.modified_count > 0:
+        print("Update successful!")
+    else:
+        print("No documents matched the filter.")
+        
+    query = {"0.name": "key"}
 
-# Execute the query
-result = mycollection.find_one(query)
+    # Execute the query
+    result = mycollection.find_one(query)
 
-# Check if the document was found
-if result:
-    # Extract the value
-    value = result['0']['value']
-    print("Retrieved value:", value)
-else:
-    print("Document not found.")
+    # Check if the document was found
+    if result:
+        # Extract the value
+        value = result['0']['value']
+        st.info("The OpenAI API Key is successfully embedded and saved!")
+    else:
+        print("Document not found.")
